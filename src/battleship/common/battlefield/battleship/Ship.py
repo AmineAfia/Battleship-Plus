aa18dc4ae5a695e924fd9caf4689c05453d50019
Flo <imperfun@gmail.com>
2017-01-17 17:06:37 +0100

    updated ship.py

---
+++ src/battleship/common/battlefield/battleship/Ship.py
@@ -0,0 +1,14 @@
+class Ship:
+
+    def __init__(self, name):
+        self._name = name
+        print("Created Ship: {}".format(name))
+
+    def shoot(xPos, yPos):
+        print("shoot at x={},y={}".format(xPos, yPos))
+
+    def move(xPos, yPos):
+        print("move at x=={},y={}".format(xPos, yPos))
+
+
+
