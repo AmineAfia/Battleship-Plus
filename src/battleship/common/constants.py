27c1fe3e76b7d5444653f63c95873fafdfeec076
Benjamin Lipp (blipp) <mail@benjaminlipp.de>
2017-01-22 22:09:46 +0100

    Move enums to contants.py

--- src/battleship/common/constants.py
+++ src/battleship/common/constants.py
@@ -1,3 +1,27 @@
+from enum import IntEnum
+
+
 class Constants:
     SERVER_PORT = 4711
     SERVER_IP = '127.0.0.1'
+
+
+class Orientation(IntEnum):
+    NORTH = 0
+    EAST = 1
+
+
+class EndGameReason(IntEnum):
+    OPPONENT_ABORT = 0
+    OPPONENT_TIMEOUT = 1
+    YOU_WON = 2
+    OPPONENT_WON = 3
+    SERVER_CLOSED_CONNECTION = 4
+    OTHER = 5
+
+
+class Direction(IntEnum):
+    NORTH = 0
+    EAST = 1
+    SOUTH = 2
+    WEST = 3
