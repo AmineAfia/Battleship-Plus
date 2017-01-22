d9807c4d5189298ba5f6df99fba089a23f6c6994
Benjamin Lipp (blipp) <mail@benjaminlipp.de>
2017-01-22 22:10:47 +0100

    More tests

--- src/battleship/client.py
+++ src/battleship/client.py
@@ -3,6 +3,7 @@ import asyncio
 import asyncio.streams
 from common.constants import Constants
 from common.protocol import ProtocolMessage, ProtocolMessageType, NumShips
+from common.constants import Orientation
 
 
 def main():
@@ -18,13 +19,11 @@ def main():
         for i in [1, 2]:
 
             yield from asyncio.sleep(1, loop=loop)
-
             login_message = ProtocolMessage(ProtocolMessageType.LOGIN,
                                             {"username": "testuser{}".format(i)})
             yield from login_message.send(writer)
 
             yield from asyncio.sleep(1, loop=loop)
-
             create_game_message = ProtocolMessage(ProtocolMessageType.CREATE_GAME,
                                             {"board_size": 5,
                                              "num_ships": NumShips([1, 2, 3, 4, 5]),
@@ -34,7 +33,14 @@ def main():
             yield from create_game_message.send(writer)
 
             yield from asyncio.sleep(1, loop=loop)
+            chat_send_message = ProtocolMessage(ProtocolMessageType.CHAT_SEND, {"username": "testuser", "text": "bummsfallerafalleri hurz"})
+            yield from chat_send_message.send(writer)
 
+            yield from asyncio.sleep(1, loop=loop)
+            move_ship_message = ProtocolMessage(ProtocolMessageType.MOVE, {"turn_counter": 2, "ship_id": 146579, "direction": Orientation.EAST})
+            yield from move_ship_message.send(writer)
+
+            yield from asyncio.sleep(1, loop=loop)
             logout_message = ProtocolMessage(ProtocolMessageType.LOGOUT)
             yield from logout_message.send(writer)
 
