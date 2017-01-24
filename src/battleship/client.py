dffe0ab48ade4c5be842c0255b83ee6f20293e54
Benjamin Lipp (blipp) <mail@benjaminlipp.de>
2017-01-24 13:47:09 +0100

    Refactior client tests

--- src/battleship/client.py
+++ src/battleship/client.py
@@ -6,6 +6,8 @@ from common.protocol import ProtocolMessage, ProtocolMessageType, NumShips
 from common.constants import Orientation, Direction, EndGameReason, ErrorCode, GameOptions
 
 
+
+
 def main():
     print("Connecting to server {}:{}".format(Constants.SERVER_IP, Constants.SERVER_PORT))
 
@@ -16,36 +18,41 @@ def main():
         reader, writer = yield from asyncio.streams.open_connection(
             Constants.SERVER_IP, Constants.SERVER_PORT, loop=loop)
 
+        @asyncio.coroutine
+        def _send_and_wait(msg: ProtocolMessage, seconds_to_wait: float=0.5):
+            yield from msg.send(writer)
+            if seconds_to_wait > 0:
+                yield from asyncio.sleep(seconds_to_wait, loop=loop)
+
         for i in [1, 2]:
 
-            yield from asyncio.sleep(1, loop=loop)
-            login_message = ProtocolMessage(ProtocolMessageType.LOGIN,
-                                            {"username": "testuser{}".format(i)})
-            yield from login_message.send(writer)
+            yield from _send_and_wait(ProtocolMessage(ProtocolMessageType.LOGIN,
+                                                        {"username": "testuser{}".format(i)}))
 
-            yield from asyncio.sleep(1, loop=loop)
-            create_game_message = ProtocolMessage(ProtocolMessageType.CREATE_GAME,
+            yield from _send_and_wait(ProtocolMessage(ProtocolMessageType.CREATE_GAME,
                                             {"board_size": 5,
                                              "num_ships": NumShips([1, 2, 3, 4, 5]),
                                              "round_time": 25,
                                              "options": GameOptions.PASSWORD,
                                              "password": "foobar"
-                                                 })
-            yield from create_game_message.send(writer)
+                                                 }))
 
-            yield from asyncio.sleep(1, loop=loop)
-            chat_send_message = ProtocolMessage(ProtocolMessageType.CHAT_SEND, {"username": "testuser", "text": "bummsfallerafalleri hurz"})
-            yield from chat_send_message.send(writer)
+            yield from _send_and_wait(ProtocolMessage(ProtocolMessageType.CREATE_GAME,
+                                            {"board_size": 5,
+                                             "num_ships": NumShips([1, 2, 3, 4, 5]),
+                                             "round_time": 25,
+                                             "options": 0
+                                                 }))
 
-            yield from asyncio.sleep(1, loop=loop)
-            move_ship_message = ProtocolMessage(ProtocolMessageType.MOVE, {"turn_counter": 2, "ship_id": 146579, "direction": Orientation.EAST})
-            yield from move_ship_message.send(writer)
+            yield from _send_and_wait(ProtocolMessage(ProtocolMessageType.CHAT_SEND, {"username": "testuser", "text": "bummsfallerafalleri hurz"}))
 
-            yield from asyncio.sleep(1, loop=loop)
-            logout_message = ProtocolMessage(ProtocolMessageType.LOGOUT)
-            yield from logout_message.send(writer)
+            yield from _send_and_wait(ProtocolMessage(ProtocolMessageType.MOVE, {"turn_counter": 2, "ship_id": 146579, "direction": Orientation.EAST}))
+            
+            yield from _send_and_wait(ProtocolMessage(ProtocolMessageType.JOIN, {"game_id": 60000, "password": "bumms"}))
+            yield from _send_and_wait(ProtocolMessage(ProtocolMessageType.JOIN, {"game_id": 60001}))
 
-        yield from asyncio.sleep(1, loop=loop)
+            yield from _send_and_wait(ProtocolMessage(ProtocolMessageType.LOGOUT))
+
         writer.close()
 
     # creates a client and connects to our server
