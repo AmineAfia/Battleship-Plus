f87739e70325683d367801aa5b234a0b0445677b
Flo <imperfun@gmail.com>
2017-01-24 10:17:50 +0100

    fixed import problems and updated client.py

--- src/battleship/client.py
+++ src/battleship/client.py
@@ -1,10 +1,16 @@
 import sys
+from common.GameController import GameController
 from common.constants import Constants
-
 
 def main():
     print("Connecting to server {}:{}".format(Constants.SERVER_IP, Constants.SERVER_PORT))
 
 
+
 if __name__ == '__main__':
+
+    myController = GameController()
+    myController.run()
+
     sys.exit(main())
+
--- src/battleship/common/GameController.py
+++ src/battleship/common/GameController.py
@@ -1,31 +1,31 @@
-from battlefield.Battlefield import Battlefield
-from battlefield.battleship.AircraftCarrier import AircraftCarrier
-from battlefield.battleship.Battleship import Battleship
-from battlefield.battleship.Cruiser import Cruiser
-from battlefield.battleship.Destroyer import Destroyer
-from battlefield.battleship.Submarine import Submarine
+from .battlefield.Battlefield import Battlefield
+from .battlefield.battleship.AircraftCarrier import AircraftCarrier
+from .battlefield.battleship.Battleship import Battleship
+from .battlefield.battleship.Cruiser import Cruiser
+from .battlefield.battleship.Destroyer import Destroyer
+from .battlefield.battleship.Submarine import Submarine
 from .constants import Orientation, Direction
 
 #Controller for Battleship+
 class GameController:
-    game_name = "Battleship+"
-    ships = []
+    #game_name = "Battleship+"
+    #ships = []
 
     turn_counter = 0
 
     #create a new battlefield
-    def createGame(length, ships):
+    def createGame(self, length, ships):
         return Battlefield(length, ships)
 
     #move your own ship on your battlefield
-    def move(battlefield, ship_id, direction):
+    def move(self, battlefield, ship_id, direction):
         if (battlefield.move(ship_id, direction)):
             print("moved ship")
         else:
             print("error - ship not moved")
 
     #strike at the coordinates on the enemy battlefield
-    def strike(battlefield, x_pos, y_pos):
+    def strike(self, battlefield, x_pos, y_pos):
         print("strike at x={},y={}".format(x_pos, y_pos))
         if (battlefield.strike(x_pos, y_pos)):
             print("got it!")
@@ -34,9 +34,8 @@ class GameController:
 
 
 
-    #here we go
-    if __name__ == "__main__":
-
+    #run the game controller
+    def run(self):
         print("creating battlefield with ships...")
 
         #size = 5x5
@@ -46,11 +45,12 @@ class GameController:
         #ships.append(Cruiser(1,0,0,1))
         #ships.append(Destroyer(2,0,0,1))
         #ships.append(Submarine(3,0,0,1))
+        ships = []
         ships.append(AircraftCarrier(4,0,0,Orientation.EAST))
 
-        battlefield = createGame(length, ships)
+        battlefield = self.createGame(length, ships)
 
-        move(battlefield,4,Direction.EAST)
+        self.move(battlefield,4,Direction.EAST)
 
         #strike(battlefield,0,0)
         #strike(battlefield,0,1)
