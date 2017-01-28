fb75ae99addc87f2fb992c6a9d8095adcd5dc361
Benjamin Lipp (blipp) <mail@benjaminlipp.de>
2017-01-28 20:33:24 +0100

    Move server class to network.py, rewrite with callbacks

--- src/battleship/common/network.py
+++ src/battleship/common/network.py
@@ -33,3 +33,73 @@ class BattleshipClient:
 
     def close(self):
         self.writer.close()
+
+
+class BattleshipServerClient:
+
+    next_client_id = 1
+
+    def __init__(self, reader, writer):
+        self.reader = reader
+        self.writer = writer
+        self.id = BattleshipServerClient.next_client_id
+        BattleshipServerClient.next_client_id += 1
+
+    async def send(self, msg: ProtocolMessage):
+        await msg.send(self.writer)
+
+
+class BattleshipServer:
+
+    def __init__(self, ip, port, loop, client_connected_callback):
+        self.ip = ip
+        self.port = port
+        self.loop = loop
+        self.client_connected_callback = client_connected_callback
+        self.server = None
+        self.clients = {}
+
+    def _accept_client(self, client_reader, client_writer):
+        """
+        This method accepts a new client connection and creates a Task
+        to handle this client.  self.clients is updated to keep track
+        of the new client.
+        """
+
+        async def internal_msg_callback(msg: ProtocolMessage):
+            await external_msg_callback(msg)
+
+        def internal_client_done(task):
+            external_disconnected_callback()
+            del self.clients[task]
+
+        # start a new Task to handle this specific client connection
+        task = asyncio.Task(parse_from_stream(client_reader, client_writer, internal_msg_callback))
+        client_obj = BattleshipServerClient(client_reader, client_writer)
+        self.clients[task] = client_obj
+        external_msg_callback, external_disconnected_callback = self.client_connected_callback(client_obj)
+        if not inspect.iscoroutinefunction(external_msg_callback):
+            raise TypeError("msg_callback must be a coroutine")
+        task.add_done_callback(internal_client_done)
+
+    def start(self):
+        """
+        Starts the TCP server, so that it listens on the specified port.
+        For each client that connects, the accept_client method gets
+        called.  This method runs the loop until the server sockets
+        are ready to accept connections.
+        """
+        self.server = self.loop.run_until_complete(
+            asyncio.streams.start_server(self._accept_client,
+                                         self.ip, self.port,
+                                         loop=self.loop))
+
+    def stop(self):
+        """
+        Stops the TCP server, i.e. closes the listening socket(s).
+        This method runs the loop until the server sockets are closed.
+        """
+        if self.server is not None:
+            self.server.close()
+            self.loop.run_until_complete(self.server.wait_closed())
+            self.server = None
--- src/battleship/server.py
+++ src/battleship/server.py
@@ -1,92 +1,42 @@
 import sys
-from common.constants import Constants, Orientation, EndGameReason, Direction
-from common.protocol import ProtocolConfig, ProtocolMessage, ProtocolMessageType, ProtocolMessageParameters, ProtocolField, parse_from_stream
-
 import asyncio
 import asyncio.streams
-
-
-class MyServer:
-
-    next_client_id = 1
-
-    def __init__(self):
-        self.server = None  # encapsulates the server sockets
-
-        # this keeps track of all the clients that connected to our
-        # server.  It can be useful in some cases, for instance to
-        # kill client connections or to broadcast some data to all
-        # clients...
-        self.clients = {} # task -> (reader, writer)
-
-    def _accept_client(self, client_reader, client_writer):
-        """
-        This method accepts a new client connection and creates a Task
-        to handle this client.  self.clients is updated to keep track
-        of the new client.
-        """
-
-        client_id = MyServer.next_client_id
-        MyServer.next_client_id += 1
-        print("< [{}] client connected".format(client_id))
-
-        async def msg_callback(msg: ProtocolMessage):
-            print("< [{}] {}".format(client_id, msg))
-            answer: ProtocolMessage = ProtocolMessage.create_single(ProtocolMessageType.CHAT_RECV, {"sender": "sender", "recipient": "recipient", "text": "fuck. you."})
-            print("> [{}] {}".format(client_id, answer))
-            await answer.send(client_writer)
-
-        # start a new Task to handle this specific client connection
-        task = asyncio.Task(parse_from_stream(client_reader, client_writer, msg_callback))
-        self.clients[task] = (client_reader, client_writer)
-
-        def client_done(task):
-            print("< [{}] client disconnected".format(client_id))
-            del self.clients[task]
-
-        task.add_done_callback(client_done)
-
-    def start(self, loop):
-        """
-        Starts the TCP server, so that it listens on the specified port.
-        For each client that connects, the accept_client method gets
-        called.  This method runs the loop until the server sockets
-        are ready to accept connections.
-        """
-        self.server = loop.run_until_complete(
-            asyncio.streams.start_server(self._accept_client,
-                                         Constants.SERVER_IP, Constants.SERVER_PORT,
-                                         loop=loop))
-
-    def stop(self, loop):
-        """
-        Stops the TCP server, i.e. closes the listening socket(s).
-        This method runs the loop until the server sockets are closed.
-        """
-        if self.server is not None:
-            self.server.close()
-            loop.run_until_complete(self.server.wait_closed())
-            self.server = None
+from common.constants import Constants
+from common.network import BattleshipServer, BattleshipServerClient
+from common.protocol import ProtocolMessage, ProtocolMessageType
 
 
 def main():
-    print("Starting server on {}:{}".format(Constants.SERVER_IP, Constants.SERVER_PORT))
-
     loop = asyncio.get_event_loop()
 
-    # creates a server and starts listening to TCP connections
-    server = MyServer()
+    def client_connected(client):
 
-    server.start(loop)
+        def client_disconnected():
+            print("< [{}] client disconnected".format(client.id))
+
+        async def msg_callback(msg: ProtocolMessage):
+            print("< [{}] {}".format(client.id, msg))
+            answer: ProtocolMessage = ProtocolMessage.create_single(ProtocolMessageType.CHAT_RECV,
+                                                                    {"sender": "sender", "recipient": "recipient",
+                                                                     "text": "fuck. you."})
+            print("> [{}] {}".format(client.id, answer))
+            await answer.send(client.writer)
+
+        print("< [{}] client connected".format(client.id))
+        return msg_callback, client_disconnected
+
+    server = BattleshipServer(Constants.SERVER_IP, Constants.SERVER_PORT, loop, client_connected)
+
+    print("Starting server on {}:{}".format(Constants.SERVER_IP, Constants.SERVER_PORT))
+    server.start()
+
     try:
         loop.run_forever()
     except KeyboardInterrupt:
         print("\nReceived SIGINT, terminating …")
         pass
 
-    # TODO: make this cleaner, run_until_complete inside the server object feels strange
-    server.stop(loop)
-    #loop.run_until_complete(server.wait_closed())
+    server.stop()
     loop.close()
     print("Bye.")
 
