4d58221b6bb4de5fcfd09285406687bfcf164248
AmineAfia <afia.amine@gmail.com>
2017-01-28 18:50:29 +0100

    battle frontend

--- frontend/game/battle.py
+++ frontend/game/battle.py
@@ -1,119 +1,175 @@
 import urwid
 import urwid.raw_display
 import urwid.web_display
-from urwid import BoxAdapter
-from urwid import SolidFill
+from pyfiglet import Figlet
+
+from result import Result
 
 
-def main():
+def unhandled(key):
+    if key == 'q':
+        raise urwid.ExitMainLoop()
 
-    # construct the shoots labels
-    shoots = [(1,1), (2,2), (3,5), (1,2), (2,1), (1,3), (3,1), (1,4)]
-    field_offset = 4
-    text_button_list = {}
+class PopUpDialog(urwid.WidgetWrap):
+    """A dialog that appears with North, South, West and East buttons """
+    signals = ['close']
 
-    for x in range(field_offset):
-        for y in range(field_offset):
-            if (x,y) in shoots:
-                text_button_list[(x,y)] = "X"
-            else:
-                text_button_list[(x,y)] = "."
+    def __init__(self):
+        # n_button = urwid.Padding(urwid.Button("North"), width=1)
+        n_button = urwid.Button("North")
+        s_button = urwid.Button("South")
+        w_button = urwid.Button("West")
+        e_button = urwid.Button("East")
 
-    def button_press(button):
-        frame.footer = urwid.AttrWrap(urwid.Text(
-            [u"Pressed: ", button.get_label()]), 'header')
+        # w, align='left', width=('relative', 100), min_width=None, left=0, right=0)
 
-    blank = urwid.Divider()
+        urwid.connect_signal(n_button, 'click',
+                             lambda button: self._emit("close"))
+        pile = urwid.Pile([n_button, urwid.Columns([w_button, e_button], 2), s_button])
+        fill = urwid.Filler(pile)
+        self.__super.__init__(urwid.AttrWrap(fill, 'popbg'))
 
-    cell = urwid.WidgetWrap(urwid.GridFlow(
-             [urwid.AttrWrap(urwid.Button(text_button_list[txt], button_press, txt),
-                 'buttn','buttnf') for txt in text_button_list],
-             2, 1, 1, 'center'))
 
-    #cell = urwid.GridFlow(
-    #        [urwid.AttrWrap(urwid.Button(text_button_list[txt], button_press, txt),
-    #            'buttn','buttnf') for txt in text_button_list],
-    #        2, 1, 1, 'center')
+class ButtonWithAPopUp(urwid.PopUpLauncher):
+    def __init__(self):
+        self.__super.__init__(urwid.Button("_"))
+        urwid.connect_signal(self.original_widget, 'click',
+                             lambda button: self.open_pop_up())
 
-    #cell = cell.rows((4,))
-    #cell.pack(4, focus=False)
+    def create_pop_up(self):
+        pop_up = PopUpDialog()
+        urwid.connect_signal(pop_up, 'close',
+                             lambda button: self.close_pop_up())
+        return pop_up
 
-    matrix = [
-        #urwid.Padding(cell, left=4, right=3, min_width=13),
-        cell,
-        blank,
-        urwid.Padding(urwid.GridFlow(
-            [urwid.AttrWrap(urwid.Button(text_button_list[txt], button_press, txt),
-                'buttn','buttnf') for txt in text_button_list],
-            2, 1, 1, 'center'),
-            left=4, right=3, min_width=13),
-        blank,
-        urwid.Padding(urwid.GridFlow(
-            [urwid.AttrWrap(urwid.Button(text_button_list[txt], button_press, txt),
-                'buttn','buttnf') for txt in text_button_list],
-            2, 1, 1, 'center'),
-            left=4, right=3, min_width=13),
-        blank,
-    ]
+    def get_pop_up_parameters(self):
+        return {'left': 0, 'top': 1, 'overlay_width': 32, 'overlay_height': 7}
 
-    widget_list = [
-        urwid.Columns([
-            urwid.Padding(urwid.Text("Opponent field"), left=2, right=0, min_width=20),
-            urwid.Pile([ urwid.Text("Your field")]),
+
+class Battle():
+    def __init__(self):
+        self._my_attribute = 0
+        self.win = Result().show_winner
+
+    def main(self):
+        ''' Variables region'''
+        shoots = [(1, 1), (2, 2), (3, 5), (1, 2), (2, 1), (1, 3), (3, 1), (1, 4)]
+        field_offset = 10
+        text_button_list = {}
+        '''End of region'''
+
+        '''Functions region'''
+
+        def button_press(self):
+            frame.footer = urwid.AttrWrap(urwid.Text(
+                [u"Pressed: ", self.get_label()]), 'header')
+
+        # function to shoot the opponents field
+        def shoot(self):
+            self.set_label('X')
+
+        # function to move ships
+        def move(self):
+            if self.get_label() == 'North':
+                self.set_label('N')
+            elif self.get_label() == 'South':
+                self.set_label('S')
+            elif self.get_label() == 'West':
+                self.set_label('W')
+            elif self.get_label() == 'East':
+                self.set_label('E')
+
+        # def forward_win():
+        #     Result.show_winner()
+        '''End of region'''
+
+        '''Rendering region'''
+        # Constructing shooting field
+        button_list = []
+        f = []
+        for x in range(field_offset):
+            for i in range(field_offset):
+                cell = urwid.AttrWrap(urwid.Button('.', on_press=shoot), 'buttn', 'buttnf')
+                button_list.append(cell)
+            zeil = urwid.GridFlow(button_list, 1, 1, 0, 'center')
+            f.append(zeil)
+            button_list.clear()
+
+        # Constructing ships field
+        ship_button_list = []
+        ship_f = []
+        for x in range(field_offset):
+            for i in range(field_offset):
+                ship_cell = ButtonWithAPopUp()
+                ship_button_list.append(ship_cell)
+
+            ship_zeil = urwid.GridFlow(ship_button_list, 1, 1, 0, 'center')
+            ship_f.append(ship_zeil)
+            ship_button_list.clear()
+
+        # insert the matrix in piles
+        shooting_pile = urwid.Pile(f)
+        ship_pile = urwid.Pile(ship_f)
+
+        # The rendered layout
+        blank = urwid.Divider()
+        widget_list = [
+            urwid.Columns([
+                urwid.Padding(urwid.Text("Opponent field"), left=2, right=0, min_width=20),
+                urwid.Pile([urwid.Text("Your field")]),
             ], 2),
-        blank,
+            blank,
 
-        urwid.Columns([
-            cell,
-            cell,
-            #urwid.Pile(
-            #    matrix
-            #),
-            ], 2, focus_column=0, min_width=4),
-        blank,
-    ]
-
-
-    header = urwid.AttrWrap(urwid.Text("TEXT FOR HEADER!!"), 'header')
-    listbox = urwid.ListBox(urwid.SimpleListWalker(widget_list))
-    frame = urwid.Frame(urwid.AttrWrap(listbox, 'body'), header=header)
-
-    palette = [
-        ('hit', 'black', 'light gray', 'bold'),
-        ('miss', 'black', 'black', ''),
-        ('untouched', 'white', 'black', ''),
-        ('body','white','black', 'standout'),
-        ('reverse','light gray','black'),
-        ('header','white','dark red', 'bold'),
-        ('important','dark blue','light gray',('standout','underline')),
-        ('editfc','white', 'dark blue', 'bold'),
-        ('editbx','light gray', 'dark blue'),
-        ('editcp','black','light gray', 'standout'),
-        ('bright','dark gray','light gray', ('bold','standout')),
-        ('buttn','black','dark cyan'),
-        ('buttnf','white','dark blue','bold'),
+            urwid.Columns([
+                shooting_pile,
+                ship_pile,
+            ], 2),
+            blank,
+            urwid.Columns([
+                urwid.Button('WIN', on_press=self.win),
+            ], 2),
         ]
 
-    # use appropriate Screen class
-    if urwid.web_display.is_web_request():
-        screen = urwid.web_display.Screen()
-    else:
-        screen = urwid.raw_display.Screen()
+        header = urwid.AttrWrap(urwid.Text("Battleship+"), 'header')
+        listbox = urwid.ListBox(urwid.SimpleListWalker(widget_list))
+        frame = urwid.Frame(urwid.AttrWrap(listbox, 'body'), header=header)
+        '''End of region'''
 
-    def unhandled(key):
-        if key == 'esc':
-            raise urwid.ExitMainLoop()
+        palette = [
+            ('hit', 'black', 'light gray', 'bold'),
+            ('miss', 'black', 'black', ''),
+            ('untouched', 'white', 'black', ''),
+            ('body', 'white', 'black', 'standout'),
+            ('reverse', 'light gray', 'black'),
+            ('header', 'white', 'dark red', 'bold'),
+            ('important', 'dark blue', 'light gray', ('standout', 'underline')),
+            ('editfc', 'white', 'dark blue', 'bold'),
+            ('editbx', 'light gray', 'dark blue'),
+            ('editcp', 'black', 'light gray', 'standout'),
+            ('bright', 'dark gray', 'light gray', ('bold', 'standout')),
+            ('buttn', 'white', 'black'),
+            ('buttnf', 'white', 'dark blue', 'bold'),
+            ('popbg', 'white', 'dark gray')
+        ]
 
-    urwid.MainLoop(frame, palette, screen,
-        unhandled_input=unhandled).run()
+        # use appropriate Screen class
+        if urwid.web_display.is_web_request():
+            screen = urwid.web_display.Screen()
+        else:
+            screen = urwid.raw_display.Screen()
 
-def setup():
-    urwid.web_display.set_preferences("Urwid Tour")
-    # try to handle short web requests quickly
-    if urwid.web_display.handle_short_request():
-        return
+        urwid.MainLoop(frame, palette, screen,
+                       unhandled_input=unhandled, pop_ups=True).run()
 
-    main()
+    def setup(self):
+        urwid.web_display.set_preferences("Urwid Tour")
+        # try to handle short web requests quickly
+        if urwid.web_display.handle_short_request():
+            return
 
-if '__main__'==__name__ or urwid.web_display.is_web_request():
-    setup()
+        self.main()
+
+
+if '__main__' == __name__ or urwid.web_display.is_web_request():
+    battle = Battle()
+    battle.setup()
--- frontend/game/result.py
+++ frontend/game/result.py
@@ -1,37 +1,44 @@
 import urwid
 from pyfiglet import Figlet
+from urwid import Button
 
-def exit_on_q(key):
-    if key in ('enter'):
-        raise urwid.ExitMainLoop()
 
-wlcm = Figlet(font='big')
+class Result:
+    def __init__(self):
+        self.wlcm = Figlet(font='big')
 
-palette = [
-    ('banner', '', '', '', '#ffa', '#60a'),
-    ('streak', '', '', '', 'g50', '#60a'),
-    ('inside', '', '', '', 'g38', '#808'),
-    ('outside', '', '', '', 'g27', '#a06'),
-    ('bg', '', '', '', 'g7', '#d06'),]
+        self.palette =[
+                ('banner', '', '', '', '#ffa', '#60a'),
+                ('streak', '', '', '', 'g50', '#60a'),
+                ('inside', '', '', '', 'g38', '#808'),
+                ('outside', '', '', '', 'g27', '#a06'),
+                ('bg', '', '', '', 'g7', '#d06'),]
 
-placeholder = urwid.SolidFill()
-loop = urwid.MainLoop(placeholder, palette, unhandled_input=exit_on_q)
-loop.screen.set_terminal_properties(colors=256)
-loop.widget = urwid.AttrMap(placeholder, 'bg')
-loop.widget.original_widget = urwid.Filler(urwid.Pile([]))
+    def exit_on_q(key, foo):
+        if key == 'q':
+            raise urwid.ExitMainLoop()
 
-div = urwid.Divider()
-outside = urwid.AttrMap(div, 'outside')
-inside = urwid.AttrMap(div, 'inside')
+    def show_winner(self, foo):
+        print(foo)
+        print(type(foo))
 
-# TODO game result to be received from client/server
-result = 'win'
-if result == 'win':
-    txt = urwid.Text(('banner', wlcm.renderText('YOU WIN !')), align='center')
+        placeholder = urwid.SolidFill()
+        loop = urwid.MainLoop(placeholder, self.palette, unhandled_input=self.exit_on_q, pop_ups=True)
+        loop.screen.set_terminal_properties(colors=256)
+        loop.widget = urwid.AttrMap(placeholder, 'bg')
+        loop.widget.original_widget = urwid.Filler(urwid.Pile([]))
 
-streak = urwid.AttrMap(txt, 'streak')
-pile = loop.widget.base_widget
-for item in [outside, inside, streak, inside, outside]:
-    pile.contents.append((item, pile.options()))
+        div = urwid.Divider()
+        outside = urwid.AttrMap(div, 'outside')
+        inside = urwid.AttrMap(div, 'inside')
 
-loop.run()
+        # TODO game result to be received from client/server in the battle screen to lunch the forward here
+        result = 'win'
+        if result == 'win':
+            txt = urwid.Text(('banner', self.wlcm.renderText('YOU WIN !')), align='center')
+            streak = urwid.AttrMap(txt, 'streak')
+            pile = loop.widget.base_widget
+            for item in [outside, inside, streak, inside, outside]:
+                pile.contents.append((item, pile.options()))
+
+        loop.run()
