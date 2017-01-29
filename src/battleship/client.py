357cede8ab7d70b9210ee23d6805f5d3249945c1
Flo <imperfun@gmail.com>
2017-01-29 16:08:53 +0100

    updated client and controller with commands

--- src/battleship/client.py
+++ src/battleship/client.py
@@ -1,6 +1,6 @@
 import sys
 from common.GameController import GameController
-from common.constants import Constants
+from common.constants import Orientation, Direction, Constants
 from common.protocol import ProtocolMessage, ProtocolMessageType
 
 
@@ -8,34 +8,55 @@ def main():
     print("Connecting to server {}:{}".format(Constants.SERVER_IP, Constants.SERVER_PORT))
 
 
-
-
 if __name__ == '__main__':
 
+    #def msg_callback(msg: ProtocolMessage):
+        #if msg.type == ProtocolMessageType.CHAT_RECV:
+            #print(msg.parameters["sender"])
+            #print(msg.parameters["recipient"])
+            #pass
 
+    my_controller = GameController()
+
+    #cmd = ""
+    #while (cmd != "exit"):
+        #cmd = input("Was du wolle?")
+        #print (cmd)
+
+    #CREATE THE BATTLEFIELD
     length = 10
-    ships = [0,0,0,0,2]
+    ships = [0, 0, 0, 0, 1]
+    cmd = ["create", length, ships]
+    my_controller.run(cmd)
 
-    def msg_callback(msg: ProtocolMessage):
-        if msg.type == ProtocolMessageType.CHAT_RECV:
-            print(msg.parameters["sender"])
-            print(msg.parameters["recipient"])
-            pass
+    #PLACE THE SHIPS
+    ship_id = 1
+    x_pos = 0
+    y_pos = 0
+    orientation = Orientation.EAST
+    cmd = ["place", ship_id, x_pos, y_pos, orientation]
+    my_controller.run(cmd)
 
-    myController = GameController()
+    # MOVE YOUR SHIP
+    ship_id = 1
+    direction = Direction.EAST
+    cmd = ["move", ship_id, direction]
+    my_controller.run(cmd)
 
-    cmd = ""
-    while (cmd != "exit"):
-        cmd = input("Was du wolle?")
-        print (cmd)
+    #STRIKE FROM ENEMY
+    x_pos = 0
+    y_pos = 0
+    cmd = ["strike", x_pos, y_pos]
+    my_controller.run(cmd)
+    
+    #SHOOT AT ENEMY BATTLEFIELD
+    x_pos = 0
+    y_pos = 0
+    cmd = ["shoot", x_pos, y_pos]
+    my_controller.run(cmd)
 
 
 
-    myController.run(length, ships)
-    myController.place(1, 0, 0, 1)
-    myController.place(2, 0, 1, 1)
-
-    myController.play()
 
     sys.exit(main())
 
--- src/battleship/common/GameController.py
+++ src/battleship/common/GameController.py
@@ -48,58 +48,84 @@ class GameController:
             else:
                 print("fail!")
 
+    #shoot at enemy battlefield
+    def shoot(self, x_pos, y_pos):
+        if (self._game_started):
+            print("shoot at x={}, y={}".format(x_pos, y_pos))
+            self._battlefield.shoot(x_pos, y_pos)
+
+
 
     #run the game controller with parameter:
     #   battlefieldlength
     #   ships_table: carrier, battleship, cruiser, destroyer, submarine
-    def run(self, length, ships_table):
+    #def run(self, length, ships_table):
+    def run(self, cmd):
 
-        if len(ships_table) == 5:
-            if (length < 27 or length > 9):
+        if (cmd[0] == "create"):
+            length = cmd[1]
+            ships_table = cmd[2]
 
-                #create ships
-                id = 0
-                ships = []
-                for i in range(5):
-                    shipCount = ships_table[i]
-                    for _ in range(shipCount):
-                        id = id + 1
-                        if (i == 0):
-                            ships.append(AircraftCarrier(id, 0, 0,Orientation.EAST))
-                        if (i == 1):
-                            ships.append(Battleship(id, 0, 0, Orientation.EAST))
-                        if (i == 2):
-                            ships.append(Cruiser(id, 0, 0,Orientation.EAST))
-                        if (i == 3):
-                            ships.append(Destroyer(id, 0, 0,Orientation.EAST))
-                        if (i == 4):
-                            ships.append(Submarine(id, 0, 0,Orientation.EAST))
+            if len(ships_table) == 5:
+                if (length < 27 or length > 9):
 
-                #create battlefield
-                self.createBattlefield(length, ships)
+                    #create ships
+                    id = 0
+                    ships = []
+                    for i in range(5):
+                        shipCount = ships_table[i]
+                        for _ in range(shipCount):
+                            id = id + 1
+                            if (i == 0):
+                                ships.append(AircraftCarrier(id, 0, 0,Orientation.EAST))
+                            if (i == 1):
+                                ships.append(Battleship(id, 0, 0, Orientation.EAST))
+                            if (i == 2):
+                                ships.append(Cruiser(id, 0, 0,Orientation.EAST))
+                            if (i == 3):
+                                ships.append(Destroyer(id, 0, 0,Orientation.EAST))
+                            if (i == 4):
+                                ships.append(Submarine(id, 0, 0,Orientation.EAST))
 
+                    #create battlefield
+                    self.createBattlefield(length, ships)
+
+                else:
+                    print("wrong length")
             else:
-                print("wrong length")
-        else:
-            print("wrong number of ships")
+                print("wrong number of ships")
 
+        # def place(self, ship_id, x_pos, y_pos, orientation):
+        if (cmd[0] == "place"):
+            ship_id = cmd[1]
+            x_pos = cmd[2]
+            y_pos = cmd[3]
+            orientation = cmd[4]
 
-    def place(self, ship_id, x_pos, y_pos, orientation):
-                self.placeShip(ship_id, x_pos, y_pos, orientation)
+            self.placeShip(ship_id, x_pos, y_pos, orientation)
 
-                #if all ships are places and the game starts
-                self.startGame()
+            #if all ships are places and the game starts
+            self.startGame()
 
+        #def move(self, ship_id, direction)
+        if (cmd[0] == "move"):
+            ship_id = cmd[1]
+            direction = cmd[2]
 
+            self.move(ship_id, direction)
 
-    def play(self):
-            self.move(1, Direction.EAST)
+        #def strike(self, x_pos, y_pos)
+        if (cmd[0] == "strike"):
+            x_pos = cmd[1]
+            y_pos = cmd[2]
 
-            self.strike(0, 0)
-            self.strike(1,0)
-            self.strike(2,0)
+            self.strike(x_pos, y_pos)
 
-#Commands: PLACE, MOVE, STRIKE, SHOOT,
+        if (cmd[0] == "shoot"):
+            x_pos = cmd[1]
+            y_pos = cmd[2]
+
+            self.shoot(x_pos, y_pos)
 
 
 
