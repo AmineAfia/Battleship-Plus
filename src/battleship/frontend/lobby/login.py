74b02af7ef3fc0e94ce5bbf7fe2a303296e15857
AmineAfia <afia.amine@gmail.com>
2017-01-30 21:22:36 +0100

    Added ships positioning and loggin

--- src/battleship/frontend/lobby/login.py
+++ src/battleship/frontend/lobby/login.py
@@ -1,3 +1,31 @@
 import urwid
 
-class
+from .lobby import Lobby
+
+class Login:
+    def __init__(self):
+
+        self.username = urwid.Edit("Username: ")
+        self.password = urwid.Edit("Password: ", mask="*")
+
+    def forward_lobby(self, key):
+        if key == 'enter':
+            creat_game = Lobby()
+            creat_game.lobby_main()
+            raise urwid.ExitMainLoop()
+
+    def login_main(self):
+
+        dialog = urwid.Columns([
+                    urwid.Text(""),
+                    urwid.LineBox(urwid.Pile([self.username, self.password]), 'Login'),
+                    urwid.Text("")
+                    ], 2)
+
+        f = urwid.Filler(dialog)
+        # f.render((1, 1))
+        urwid.MainLoop(f, unhandled_input=self.forward_lobby).run()
+
+if '__main__' == __name__:
+    l = Login()
+    l.login_main()
