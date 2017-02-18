eb7a1645467d8a5f8a062933cb996b7a40747fe2
Flo <imperfun@gmail.com>
2017-02-18 15:19:36 +0100

    only testscript updates

--- src/battleship/auto_test.py
+++ src/battleship/auto_test.py
@@ -8,6 +8,9 @@ from common.constants import Orientation, Direction, EndGameReason, ErrorCode, G
 from common.network import BattleshipClient
 from common.protocol import ProtocolMessage, ProtocolMessageType, ShipPositions, Position, Positions, ShipPosition, NumShips
 
+def get_cmd()
+    return ["chat", "user1", "NOOB"]
+
 
 def main():
     print("Connecting to server {}:{}".format(Constants.SERVER_IP, Constants.SERVER_PORT))
@@ -31,56 +34,33 @@ def main():
         # Ding was die Verbindung managed zum Server
         battleship_client = BattleshipClient(loop, msg_callback, closed_callback)
         await battleship_client.connect(Constants.SERVER_IP, Constants.SERVER_PORT)
+
+        # Noch son Ding was die Verbindung managed zum Server
+        battleship_client_one = BattleshipClient(loop, msg_callback, closed_callback)
+        await battleship_client_one.connect(Constants.SERVER_IP, Constants.SERVER_PORT)
+
+
         # the following messages are just to test
         # normally you can just call `await battleship_client.send(msg)`
         # await is necessary because it's asynchronous
 
         await _send_and_wait(ProtocolMessage.create_single(ProtocolMessageType.LOGIN,
-                                                    {"username": "testuser2".format(client_id)}))
+                                                    {"username": "testuser1".format(client_id)}))
 
-        input("PUSH THE BUTTON for chatting")
-        await _send_and_wait(ProtocolMessage.create_single(ProtocolMessageType.CHAT_SEND, {"username": "testuser1".format((client_id + 1) % 2), "text": "Hi Loser, Grüße von client {}".format(client_id)}))
-        input("PUSH THE BUTTON to join a game")
-        await _send_and_wait(ProtocolMessage.create_single(ProtocolMessageType.JOIN, {"game_id": 1}))
+        await _send_and_wait(ProtocolMessage.create_single(ProtocolMessageType.CHAT_SEND, {"username": "testuser2".format((client_id + 1) % 2), "text": "client {} schießt den server ab :P".format(client_id)}))
 
-        input("PUSH THE BUTTON to place a game")
-        msg = ProtocolMessage.create_single(ProtocolMessageType.PLACE,
-                                            {"ship_positions": ShipPositions([
-                                                ShipPosition(Position(0, 0), Orientation.EAST)])})
+        #await _send_and_wait(ProtocolMessage.create_single(ProtocolMessageType.CHAT_SEND, {"username": "", "text": "z"}))
 
-        await _send_and_wait(msg)
+
 	####################################################################################################################
 	# Start Playing #
 	####################################################################################################################
-        end = input("x=exit, m=move, s=shoot:_")
-        while end is not "x":
-            if end == "s":
-                turn_counter = int(input("insert turn_counter to shoot: "))
-                x_pos = int(input("shot x_pos: "))
-                y_pos = int(input("shoot y_pos: "))
-                msg = ProtocolMessage.create_single(ProtocolMessageType.SHOOT,
-                     			{"position": Position(y_pos, x_pos),
-                	     		"turn_counter": turn_counter})
 
-            if end == "m":    
-                turn_counter = int(input("insert turn_counter to move: "))
-                msg = ProtocolMessage.create_single(ProtocolMessageType.MOVE,
-                                           {"ship_id": 0, "direction": Direction.EAST,
-                                            "turn_counter": turn_counter})
-
-
-            await _send_and_wait(msg)
-            end = input("x=exit, m=move, s=shoot:_")
-
-	#ABORT
-        input("PUSH THE BOTTEN for abort")
-        msg = ProtocolMessage.create_single(ProtocolMessageType.ABORT)
-        await _send_and_wait(msg)
 	####################################################################################################################
 	# Logout #
 	####################################################################################################################
-        input("PUSH THE BUTTON to logout")
         await _send_and_wait(ProtocolMessage.create_single(ProtocolMessageType.LOGOUT))
+
         battleship_client.close()
 
     # creates a client and connects to our server
